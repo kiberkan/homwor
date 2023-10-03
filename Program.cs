f151dc966a977601ffad873c1d9f07040f7d4d37
Anton <kiberkan@gmail.com>
2023-10-03 17:02:49 +0700

    Написал 3 метода(создание. заполнение. печать)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+int[] CreateArray(int count) // Метод создания массива
+{
+    return new int[count];
+}
+void FillArray(int[] array, int minValue, int maxValue) // метод заполнения массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+}
+string PrintArray(int[] array) // метод печать массива
+{
+    int count = array.Length;
+    string res = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    return res;
+}
+
